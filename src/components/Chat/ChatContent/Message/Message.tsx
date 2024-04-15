815769cf37a53798795df2e4e9812584d30e9d50
yuzhi <791517865@qq.com>
2024-04-15 20:08:47 +0800

    UI调整：固定输入框高度

--- src/components/Chat/ChatContent/Message/Message.tsx
+++ src/components/Chat/ChatContent/Message/Message.tsx
@@ -35,7 +35,7 @@ const Message = React.memo(
         }`}
       >
         <div
-          className={`text-base gap-4 md:gap-6 m-auto p-4 md:py-6 transition-all ease-in-out ${
+          className={`h-full text-base gap-4 md:gap-6 m-auto p-4 md:py-6 transition-all ease-in-out ${
             hideSideMenu
               ? 'md:max-w-5xl lg:max-w-5xl xl:max-w-6xl'
               : 'md:max-w-3xl lg:max-w-3xl xl:max-w-4xl'
